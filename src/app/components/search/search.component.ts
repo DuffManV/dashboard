c19c37a06707266866c531d2f072b67179b42956
vkozlov <abcr210sDF>
2024-08-22 15:38:13 +0300

    Addes button ui component and fixed styles

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {InputTextModule} from "primeng/inputtext";
 import {FormsModule} from "@angular/forms";
+import {ButtonComponent} from "../../ui/button/button.component";
 
 @Component({
   selector: 'app-search',
   standalone: true,
   imports: [
     InputTextModule,
-    FormsModule
+    FormsModule,
+    ButtonComponent
   ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
--- src/app/ui/button/button.component.ts
+++ src/app/ui/button/button.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {Button, ButtonModule} from 'primeng/button';
+
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [
+    Button
+  ],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
