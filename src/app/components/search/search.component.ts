79661af9b957576a835a612cf3d750ffa62bfaab
vkozlov <abcr210sDF>
2024-09-11 00:17:33 +0300

    Fixes

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,9 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../button/button.component';
 import { Button } from 'primeng/button';
-import { ActivatedRoute, provideRouter, Router } from '@angular/router';
-import * as test from 'node:test';
-import { serveWebpackBrowser } from '@angular-devkit/build-angular/src/builders/dev-server/webpack-server';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -15,20 +13,15 @@ import { serveWebpackBrowser } from '@angular-devkit/build-angular/src/builders/
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
-  @ViewChild('searchForm') form: any;
   protected readonly searchIcon: string = 'pi pi-search';
   protected readonly cssClass: string = 'button-search';
   public searchString: string = '';
 
-  public onSubmit(): void {
-    this.router.navigate(['search/', this.searchString]);
-  }
-  public changeInput = (): void => {
-    console.log(this.searchString);
-  };
-
   constructor(private router: Router) {}
   public clickHandler = (): void => {
-    this.router.navigate(['search/', this.searchString]);
+    this.router.navigate(['search', this.searchString]);
+  };
+  public changeInput = (): void => {
+    console.log(this.searchString);
   };
 }
